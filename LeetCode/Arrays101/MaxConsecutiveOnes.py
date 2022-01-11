986ce7a297b6515a35fa8aa8ef99cdd6e34e7093
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2022-01-11 20:26:01 -0300

    Max Consecutive ones

--- LeetCode/Arrays101/MaxConsecutiveOnes.py
+++ LeetCode/Arrays101/MaxConsecutiveOnes.py
@@ -1,3 +1,20 @@
+from typing import List
+
+
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        
+        consec = 0
+        consecMax = consec
+
+        for num in nums:
+            if num == 1:
+
+                consec = consec + 1
+
+                if consec > consecMax:
+                    consecMax = consec
+
+            elif num == 0:
+                consec = 0
+
+        return consecMax
