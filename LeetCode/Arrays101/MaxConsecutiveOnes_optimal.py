4f03da6e0f6c4150fc3e7cfaf83b231c0171d9b8
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2022-01-12 09:58:13 -0300

    adding new folder for blind75

--- LeetCode/Arrays101/MaxConsecutiveOnes_optimal.py
+++ LeetCode/Arrays101/MaxConsecutiveOnes_optimal.py
@@ -5,4 +5,6 @@ from typing import List
 
 class Solution:
     def findMaxConsecutiveOnes1(self, nums: List[int]) -> int:
-  
+        maxHere, theMax = 0, 0
+        for num in nums:
+            theMax = max(theMax, maxHere=num == 0 ? 0: maxHere+1)
--- LeetCode/Arrays101/SquaresOfASortedArray.py
+++ LeetCode/Arrays101/SquaresOfASortedArray.py
@@ -1,6 +1,9 @@
 from typing import List
 
+# Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted
+# in non-decreasing order.
+# Squaring each element and sorting the new array is very trivial
+# could you find an O(n) solution using a different approach?
 
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        
