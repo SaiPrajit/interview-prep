59a13cd759dd8edf6a75d817b6482d0259b686af
Muramatsu2602 <pedromuramatsuc@gmail.com>
2022-01-15 23:44:03 -0300

    adding a python solution to 27

---
+++ LeetCode/Arrays101/RemoveElement-optimal.py
@@ -0,0 +1,14 @@
+# https://www.youtube.com/watch?v=Pcd1ii9P9ZI&ab_channel=NeetCode
+# k --> number of values that are not val
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+
+        for i in range(len(nums)):
+            # performing the swap
+            if nums[i] != val:
+                # partition
+                nums[k] = nums[i]
+                k += 1
+
+        return k
