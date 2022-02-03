cf493634bad692479f66ef10ab26cb0e1790ac16
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2022-02-02 23:15:46 -0300

    removing coursera python folder

--- CourseraPython/course1-basics/loops/forloop.py
+++
@@ -1,9 +0,0 @@
-for i in [5,4,3,2,1] :
-    print(i)
-print('Testing')
-
-# list of strings
-friends = ['Matsumoto','Ryuchi','Ryosuke']
-for friend in friends :
-    print("JAPANESE: "+friend)
-    
--- CourseraPython/course2-data-structures/hey.py
+++
@@ -1,2 +0,0 @@
-print("data structs baby!!")
-# asdadadadadadad
